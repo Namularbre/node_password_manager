d8c398d65211cdde1714fb1e82343ed34c629b8c
Thomas <namulabre@gmail.com>
2024-01-10 19:45:39 +0100

    Added missing jsdoc in log function

--- middlewares/log.js
+++ middlewares/log.js
@@ -1,3 +1,10 @@
+/**
+ *
+ * @param req {Request}
+ * @param res {Response}
+ * @param next {Function}
+ * @returns {Promise<void>}
+ */
 async function log(req, res, next) {
     console.log(`${new Date(Date.now()).toDateString()}: Route: ${req.url}, method: ${req.method}`);
     next();

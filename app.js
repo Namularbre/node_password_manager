ac929823509d84f9637deb6499d016dcd49335bf
Thomas <namulabre@gmail.com>
2024-01-18 19:45:27 +0100

    app: Made listening host/port configurable

--- app.js
+++ app.js
@@ -26,6 +26,9 @@ app.get('/', log, (req, res) => {
     res.send({message: 'ok'});
 });
 
-app.listen(3000, () => {
+const host = process.env.HOST || "127.0.0.1";
+const port = process.env.PORT || 3000;
+
+app.listen(port, host, () => {
     console.log(`listening on http://localhost:3000`);
 });

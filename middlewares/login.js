3eac37e0711e29bb135c7752610177c086c43502
Thomas <namulabre@gmail.com>
2024-01-27 01:06:11 +0100

    login: updated middleware to use jwt

--- middlewares/login.js
+++ middlewares/login.js
@@ -1,45 +1,29 @@
 const UserModel = require('../models/userModel');
-const hashing = require('../utils/hashing');
+const jwt = require('jsonwebtoken');
 
 /**
  *
  * @param req {Request}
  * @param res {Response}
- * @param next {Function}
+ * @param next {function}
  * @returns {Promise<void>}
  */
 async function login(req, res, next) {
-    let username, password, email;
+    const token = req.headers['authorization'];
 
-    if (req.body) {
-        username = req.body.username;
-        password = req.body.password;
-        email = req.body.email;
-    }
-
-    if (!username && req.headers.username) {
-        username = req.headers.username;
-        password = req.headers.password;
-        email = req.headers.email;
-    }
-
-    if (username && password && email) {
-        const user = await UserModel.find(username, email);
-
-        if (user.length !== 0) {
-            const hashedPassword = user[0].password;
-
-            if (await hashing.compare(password, hashedPassword)) {
-                next();
-            } else {
-                res.status(401).send({message: "Unknown user."});
-            }
-        } else {
-            res.status(401).send({message: "Unknown user."});
-        }
-
+    if (token == null) {
+        res.sendStatus(401);
     } else {
-        res.status(401).send({message: "You need to be connected in order to do this command"});
+        jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
+            if (err) {
+                console.error(err);
+                return res.sendStatus(403);
+            }
+
+            req.user = user;
+
+            next();
+        });
     }
 }
 

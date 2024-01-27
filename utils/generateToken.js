9de5250e8c905428a3ec76c536da69404a6a257f
Thomas <namulabre@gmail.com>
2024-01-27 01:05:58 +0100

    generateToken: Added email to token information

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
-async function generateToken(username) {
-    return  jwt.sign({ username }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+async function generateToken(username, email) {
+    return  jwt.sign({ username, email }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
 }
 
 module.exports = {generateToken};

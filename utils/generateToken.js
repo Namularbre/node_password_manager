a1fba624c079e1c93de1efbe7ac7e0116bbb4e10
Thomas <namulabre@gmail.com>
2024-01-30 14:35:36 +0100

    generateToken: Added function description

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,5 +1,11 @@
 const jwt = require('jsonwebtoken');
 
+/**
+ * Generate a new JWT from username and email.
+ * @param username {string}
+ * @param email {string}
+ * @returns {Promise<*>}
+ */
 async function generateToken(username, email) {
     return  jwt.sign({ username, email }, process.env.TOKEN_SECRET, { expiresIn: '1h' });
 }
